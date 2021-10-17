0a4927a13e805f4ad353955d566c8bcc8ae0aa58
Craig Chisholm <craig.chisholm@protonmail.com>
2021-10-17 16:30:03 +0200

    Script to search for all .tex files and extract information about "\label"s + update README.md

---
+++ GetRefs.py
@@ -0,0 +1,46 @@
+#Script to get all labels in TeX project
+
+import glob as glob
+import numpy as np
+
+def ExtractLabelInfo(lineNum):
+    info = {}
+    line = allText[lineNum]
+    try:
+        info['Type'] = line.split('\label{')[1].split(':')[0]
+        info['Key'] = line.split('\label{'+info['Type']+':')[1].split('}')[0]
+        if (info['Type'] in ['fig','tab']):
+            info['Caption'] = line.split('\label{'+info['Type']+':'+info['Key']+'}')[1].split('}')[0]
+        elif (info['Type']=='sec'):
+            try:
+                sectionLine = allText[lineNum-1]
+                info['Section'] = sectionLine.split('{')[1].split('}')[0]
+                info['Section Type'] = sectionLine.split('\\')[1].split('{')[0]
+            except IndexError:
+                pass
+    except IndexError:
+        info = {}
+        info['Label'] = line.split('\label{')[1].split('}')[0]
+    info['File'] = fileMarkers[keysList[np.where(np.array(keysList)<lineNum)[0][-1]]]
+    return info
+
+#Specify directory of project
+directory = ''
+#Search for all .tex files in main directory and subdirectories
+texFiles = glob.glob(directory+'**/*.tex',recursive=True)
+#Get every line of text into one list with a dictionary marking
+# indices of beginnings of files
+allText = []
+fileMarkers = {}
+for texFile in texFiles:
+    fileMarkers[len(allText)] = texFile.split(directory)[-1]
+    with open(texFile,'r') as f:
+        allText.extend(f.readlines())
+keysList = list(fileMarkers.keys())
+#Find all lines containg \label
+labelLines = [II for II, line in enumerate(allText) if '\label' in line and not line[0]=='%']
+#Extract label info
+labels = [ExtractLabelInfo(labelLine) for labelLine in labelLines]
+for label in labels:
+    print(label)
+    print('\n')
